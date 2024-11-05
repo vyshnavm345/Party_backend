1664a74549de69e4d0e315fd005331178e82d2c8
Vishnu M <vyshnav.m345@gmail.com>
2024-11-05 16:32:07 +0530

    Updated settings

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "udv",
+        "USER": "udv_user",
+        "PASSWORD": "yU7xYaJz56mUxGh",
+        "PORT": "5432",
+        "HOST": "localhost",
     }
 }
 

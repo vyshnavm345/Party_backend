95e80987fddb3657b72a02a5fe4334f408aa4d57
Vishnu M <vyshnav.m345@gmail.com>
2024-11-05 19:37:02 +0530

    Set up environment-variables

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "udv",
-        "USER": "udv_user",
-        "PASSWORD": "yU7xYaJz56mUxGh",
-        "PORT": "5432",
-        "HOST": "localhost"
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,10 +1,12 @@
+import os
+
 from .base import *  # noqa: F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h$@e96=hy*fn*k#pu=738t6&z#oer4s_q@nkiqtk0v!+l-_km2"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
